a2ef9dcf5394e51422491014d7a465df57febeb8
aerlingsson <arnar.h.erlingsson@gmail.com>
2024-12-01 19:52:27 +0100

    Array to tuple

--- src/Day1.fs
+++ src/Day1.fs
@@ -5,12 +5,13 @@ open System
 let private parse (s: string) =
   s.Split "\r\n"
   |> Array.filter ((<>) String.Empty)
-  |> (Array.map (fun x -> x.Split "   " |> Array.map int))
+  |> Array.map (fun x -> x.Split "   " |> Array.map int)
+  |> Array.map (fun xs -> xs[0], xs[1])
 
 let part1 (input: string) =
   input
   |> parse
-  |> Array.fold (fun (xs, ys) row -> row[0] :: xs, row[1] :: ys) ([], [])
+  |> Array.fold (fun (xs, ys) (x, y) -> x :: xs, y :: ys) ([], [])
   |> fun (xs, ys) -> (List.sort xs, List.sort ys)
   ||> List.zip
   |> List.map (fun (x, y) -> abs (x - y))
@@ -26,8 +27,8 @@ let part2 (input: string) =
     |> Array.map (fun (x, xs) -> x, xs.Length)
     |> dict
 
-  let xFreqs = freqs Array.head parsed
-  let yFreqs = freqs (Array.item 1) parsed
+  let xFreqs = freqs fst parsed
+  let yFreqs = freqs snd parsed
 
   xFreqs.Keys
   |> Seq.toArray
