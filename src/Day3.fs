1f68ced156f9cd37792bc129602413a644821cc7
aerlingsson <arnar.h.erlingsson@gmail.com>
2024-12-03 20:30:18 +0100

    Day3 part2

--- src/Day3.fs
+++ src/Day3.fs
@@ -3,12 +3,46 @@ module Day3
 open System
 open System.Text.RegularExpressions
 
-let private regex = Regex "mul\((\d+),(\d+)\)"
+type Command =
+  | Mul of int * int
+  | Do
+  | Dont
+
+// Matches "do" or "don't" or "mul(1, 2)" and captures them as ["do"], ["don't"] and ["1"; "2"]
+let private regex = Regex $"(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)"
 
 let private parse (s: string) =
   regex.Matches s
   |> Seq.toList
-  |> List.map (fun x -> int x.Groups[1].Value, int x.Groups[2].Value)
+  |> List.map (fun x ->
+    let groups =
+      x.Groups
+      |> Seq.tail
+      |> Seq.filter (fun g -> g.Value <> String.Empty)
+      |> Seq.toList
+
+    match groups with
+    | [ a; b ] -> Mul(int a.Value, int b.Value)
+    | [ a ] when a.Value = "do()" -> Do
+    | [ a ] when a.Value = "don't()" -> Dont
+    | _ -> failwith "Expected only one or two captured groups")
 
 let part1 (input: string) =
-  input |> parse |> List.sumBy (fun (a, b) -> a * b)
+  input
+  |> parse
+  |> List.sumBy (fun x ->
+    match x with
+    | Mul(a, b) -> a * b
+    | _ -> 0)
+
+let part2 (input: string) =
+  input
+  |> parse
+  |> List.fold
+    (fun (active, sum) command ->
+      match command with
+      | Mul(a, b) -> if active then (active, (a * b) + sum) else (active, sum)
+      | Do -> (true, sum)
+      | Dont -> (false, sum))
+    (true, 0)
+  |> snd
--- test/Day3Test.fs
+++ test/Day3Test.fs
@@ -4,12 +4,12 @@ open Xunit
 
 [<Fact>]
 let part1 () =
-  let ex = Day3.part1 Day3Input.exampleInput
+  let ex = Day3.part1 Day3Input.exampleInputPart1
   Assert.Equal(161, ex)
   printfn "%A" (Day3.part1 Day3Input.realInput)
 
-//[<Fact>]
-//let part2 () =
-//  let ex = Day3.part2 Day3Input.exampleInput
-//  Assert.Equal(4, ex)
-//  printfn "%A" (Day3.part2 Day3Input.realInput)
+[<Fact>]
+let part2 () =
+  let ex = Day3.part2 Day3Input.exampleInputPart2
+  Assert.Equal(48, ex)
+  printfn "%A" (Day3.part2 Day3Input.realInput)
