b14b515068558bbba0e0075e2952b25e72b89ec6
aerlingsson <arnar.h.erlingsson@gmail.com>
2024-12-04 20:56:15 +0100

    Day4 part2

--- src/Day4.fs
+++ src/Day4.fs
@@ -1,16 +1,8 @@
 module Day4
 
-let inline (.+) (a: int * int) (b: int * int) = ((fst a + fst b), (snd a + snd b))
+open System.Collections.Generic
 
-let private validOffsets =
-  [ (-1, 0), (-2, 0), (-3, 0) // up
-    (1, 0), (2, 0), (3, 0) // down
-    (0, -1), (0, -2), (0, -3) // left
-    (0, 1), (0, 2), (0, 3) // right
-    (-1, 1), (-2, 2), (-3, 3) // up + right
-    (-1, -1), (-2, -2), (-3, -3) // up + left
-    (1, -1), (2, -2), (3, -3) // down + left
-    (1, 1), (2, 2), (3, 3) ] // down + right
+let inline (.+) (a: int * int) (b: int * int) = ((fst a + fst b), (snd a + snd b))
 
 let private parse (s: string) =
   s.Split "\n"
@@ -22,13 +14,24 @@ let private parse (s: string) =
     |> Array.map (fun (colIdx, char) -> (rowIdx, colIdx), char))
   |> dict
 
-let part1 (input: string) =
-  let charPositions = input |> parse
+let isCharMatch (charPositions: IDictionary<int * int, char>) offset key char =
+  match charPositions.TryGetValue(offset .+ key) with
+  | true, c when c = char -> true
+  | _ -> false
 
-  let isMatch offset key char =
-    match charPositions.TryGetValue(offset .+ key) with
-    | true, c when c = char -> true
-    | _ -> false
+let part1 (input: string) =
+  let validOffsets =
+    [ (-1, 0), (-2, 0), (-3, 0) // up
+      (1, 0), (2, 0), (3, 0) // down
+      (0, -1), (0, -2), (0, -3) // left
+      (0, 1), (0, 2), (0, 3) // right
+      (-1, 1), (-2, 2), (-3, 3) // up + right
+      (-1, -1), (-2, -2), (-3, -3) // up + left
+      (1, -1), (2, -2), (3, -3) // down + left
+      (1, 1), (2, 2), (3, 3) ] // down + right
+
+  let charPositions = input |> parse
+  let isCharMatch = isCharMatch charPositions
 
   charPositions
   |> Seq.filter (fun x -> x.Value = 'X')
@@ -36,7 +39,30 @@ let part1 (input: string) =
     (fun (count: int) x ->
       let matches =
         validOffsets
-        |> Seq.filter (fun (m, a, s) -> isMatch m x.Key 'M' && isMatch a x.Key 'A' && isMatch s x.Key 'S')
+        |> List.filter (fun (m, a, s) -> isCharMatch m x.Key 'M' && isCharMatch a x.Key 'A' && isCharMatch s x.Key 'S')
 
       count + Seq.length matches)
+    0
+
+let part2 (input: string) =
+  let offsetPairs = [ ((-1, -1), (1, 1)), ((1, -1), (-1, 1)) ]
+
+  let charPositions = input |> parse
+  let isCharMatch = isCharMatch charPositions
+
+  let isPairMatch key (a, b) =
+    (isCharMatch a key 'M' && isCharMatch b key 'S')
+    || (isCharMatch b key 'M' && isCharMatch a key 'S')
+
+  charPositions
+  |> Seq.filter (fun x -> x.Value = 'A')
+  |> Seq.fold
+    (fun (count: int) x ->
+      if
+        offsetPairs
+        |> List.exists (fun (pair1, pair2) -> isPairMatch x.Key pair1 && isPairMatch x.Key pair2)
+      then
+        count + 1
+      else
+        count)
     0
--- test/Day4Test.fs
+++ test/Day4Test.fs
@@ -8,8 +8,8 @@ let part1 () =
   Assert.Equal(18, ex)
   printfn "%A" (Day4.part1 Day4Input.realInput)
 
-//[<Fact>]
-//let part2 () =
-//  let ex = Day4.part2 Day4Input.exampleInput
-//  Assert.Equal(48, ex)
-//  printfn "%A" (Day4.part2 Day4Input.realInput)
+[<Fact>]
+let part2 () =
+  let ex = Day4.part2 Day4Input.exampleInput
+  Assert.Equal(9, ex)
+  printfn "%A" (Day4.part2 Day4Input.realInput)
